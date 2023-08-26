5840225ef3ea6ea05fee7622ad231ae467372f2b
FKnorring <fille.vk@gmail.com>
2023-08-26 11:32:52 +0200

    added social icons

--- app/page.js
+++ app/page.js
@@ -1,12 +1,23 @@
 import Noise from "@/components/Noise";
 import Image from "next/image";
+import { TbBrandInstagram, TbBrandSpotify, TbMail } from "react-icons/tb";
 
 export default function Home() {
   return (
     <>
       <Noise />
-      <main className="w-100 h-screen pt-20 flex justify-center items-start">
+      <main className="w-100 h-screen pt-20 gap-10 flex flex-col justify-start items-center">
         <Image src="/folket.svg" alt="Folket" width={80} height={80} />
+        <div className="flex gap-10">
+          <TbBrandSpotify size={32} role="button" />
+          <a
+            role="button"
+            href="https://www.instagram.com/folket.band/"
+            aria-label="Folket Instagram"
+          >
+            <TbBrandInstagram size={32} role="button" />
+          </a>
+        </div>
       </main>
     </>
   );
