87ef6345340b2a606fac099eb554310bffd545c2
FKnorring <fille.vk@gmail.com>
2023-08-26 22:17:30 +0200

    added shadcn

--- app/page.js
+++ app/page.js
@@ -4,12 +4,13 @@ import Image from "next/image";
 import members from "@/static/members.json";
 import MemberCard from "@/components/MemberCard";
 import Carousel from "@/components/Carousel";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
     <>
       <Noise />
-      <main className="w-100 h-screen pt-20 gap-10 flex flex-col justify-start items-center overflow-y-scroll scroll-smooth overflow-x-hidden">
+      <main className="w-100 h-screen pt-20 gap-10 flex flex-col justify-start items-center overflow-x-hidden overflow-y-auto scroll-smooth ">
         <Image
           className="svg-glow"
           src="/folket.svg"
@@ -18,7 +19,9 @@ export default function Home() {
           height={80}
         />
         <Socials />
+
         <div className="mb-10" />
+
         <h1 className="text-center text-4xl mb-5 flex items-center gap-2 select-none">
           Vi är{" "}
           <span className="text-5xl font-semibold display italic svg-glow">
