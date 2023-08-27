5f62bfd5be415456bf79ebea5bc24b7f2bd15e61
FKnorring <fille.vk@gmail.com>
2023-08-27 11:43:12 +0200

    fix bottom spacing

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
     <>
       <Noise />
       <main className="w-full h-screen">
-        <div className="w-full h-full overflow-y-scroll py-20 flex flex-col gap-10 items-center text-white">
+        <div className="w-full h-full overflow-y-scroll pt-20 pb-40 flex flex-col gap-10 items-center text-white">
           <Image
             className="svg-glow"
             src="/folket.svg"
@@ -32,7 +32,7 @@ export default function Home() {
               FOLKET
             </span>
           </h1>
-          <div className="w-full">
+          <div className="w-full mb-20">
             <Carousel members={Object.values(members)} />
           </div>
         </div>
